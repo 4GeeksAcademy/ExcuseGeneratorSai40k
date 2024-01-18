fb4851e7a6adc2fe27e19dd22648ef9592d1232f
Sai40k <151876367+Sai40k@users.noreply.github.com>
2024-01-18 20:44:43 +0000

    ExcuseGen

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//----------------------------------------//
+let who = [" The dog", " My grandma", " His turtle", " My bird"];
+let action = [" ate", " peed", " crushed", " broke"];
+let what = [" my homework", " the keys", " the car"];
+let when = [
+  " before the class",
+  " right on time",
+  " when I finished",
+  " during my lunch",
+  " while I was praying"
+];
+//---------------------------------------//
+window.onload = function excuseGen() {
+  let a = Math.floor(Math.random() * 4);
+  let b = Math.floor(Math.random() * 4);
+  let c = Math.floor(Math.random() * 4);
+  let d = Math.floor(Math.random() * 4);
+  let excusa = who[a] + action[b] + what[c] + when[d];
+  var excuse = document.getElementById("cambio");
+  excuse.innerHTML = excusa;
+  console.log(excusa);
 };
